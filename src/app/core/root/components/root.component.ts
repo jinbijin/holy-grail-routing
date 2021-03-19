45f0cb30b3d53524743279aed08c58d783de4575
Jinbi Jin <jinbijin@jinbijin.nl>
2021-03-19 20:56:02 +0000

    Add holy grail layout

--- src/app/core/root/components/root.component.ts
+++ src/app/core/root/components/root.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'hgr-root',
   templateUrl: './root.component.html',
+  styleUrls: ['./root.component.scss'],
 })
 export class RootComponent {}
