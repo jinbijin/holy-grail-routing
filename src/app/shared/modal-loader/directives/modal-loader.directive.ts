c04fa34266a7acd6aad1598d0a81fcc007682846
Jinbi Jin <jinbijin@jinbijin.nl>
2021-03-20 11:52:56 +0000

    Add logic to remove modal part from route

--- src/app/shared/modal-loader/directives/modal-loader.directive.ts
+++ src/app/shared/modal-loader/directives/modal-loader.directive.ts
@@ -1,18 +1,16 @@
-import { Directive, Input, OnInit, TemplateRef } from '@angular/core';
+import { Directive, OnInit, TemplateRef } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { tap } from 'rxjs/operators';
 
 @Directive({ selector: '[hgrModalLoader]' })
 export class ModalLoaderDirective implements OnInit {
-  @Input('hgrModalLoader') returnLink!: any[];
-
   private dialogRef?: MatDialogRef<any>;
 
   constructor(
-    private readonly activatedRoute: ActivatedRoute,
     private readonly templateRef: TemplateRef<any>,
     private readonly router: Router,
+    private readonly activatedRoute: ActivatedRoute,
     private readonly matDialog: MatDialog
   ) {}
 
@@ -22,9 +20,31 @@ export class ModalLoaderDirective implements OnInit {
       .afterClosed()
       .pipe(
         tap(() => {
-          this.router.navigate(this.returnLink);
+          this.router.navigate([{ outlets: { modal: null } }], { relativeTo: this.routeWithoutModal });
         })
       )
       .subscribe();
+  }
+
+  private get routeWithoutModal(): ActivatedRoute {
+    const baseRoute = this.findModalPart(this.activatedRoute);
+    if (baseRoute.parent) {
+      return baseRoute.parent;
+    }
+
+    throw new Error('Modal route has no parent.');
+  }
+
+  private findModalPart(activatedRoute: ActivatedRoute): ActivatedRoute {
+    const snapshot = activatedRoute.snapshot;
+    if (snapshot.outlet === 'modal') {
+      return activatedRoute;
+    }
+
+    if (snapshot.url.length === 0 && activatedRoute.parent) {
+      return this.findModalPart(activatedRoute.parent);
+    }
+
+    throw new Error('Cannot find modal outlet part of route without throwing away route data.');
   }
 }
