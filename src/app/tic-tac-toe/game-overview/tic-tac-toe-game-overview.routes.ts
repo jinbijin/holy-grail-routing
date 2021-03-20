5e06bc6eb8ee3f8e4b1df9d147b480f4ad2d05e5
Jinbi Jin <jinbijin@jinbijin.nl>
2021-03-20 15:53:48 +0000

    Add overview component

---
+++ src/app/tic-tac-toe/game-overview/tic-tac-toe-game-overview.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { TicTacToeGameOverviewComponent } from './tic-tac-toe-game-overview.component';
+
+export const TIC_TAC_TOE_GAME_OVERVIEW_ROUTES: Routes = [{ path: '', component: TicTacToeGameOverviewComponent }];
--- src/app/tic-tac-toe/tic-tac-toe.routes.ts
+++ src/app/tic-tac-toe/tic-tac-toe.routes.ts
@@ -6,4 +6,10 @@ export const TIC_TAC_TOE_ROUTES: Routes = [
     path: 'overview',
     loadChildren: () => import('./game-detail/tic-tac-toe-game-detail.module').then((m) => m.TicTacToeGameDetailModule),
   },
+  {
+    path: '',
+    outlet: 'right',
+    loadChildren: () =>
+      import('./game-overview/tic-tac-toe-game-overview.module').then((m) => m.TicTacToeGameOverviewModule),
+  },
 ];
